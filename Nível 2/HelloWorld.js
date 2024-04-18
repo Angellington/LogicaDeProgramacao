7792763ba6ec6788fd008e2a1789897b1e30a2c6
Yukenshi7230 <61129075+Yukenshi7230@users.noreply.github.com>
2024-04-17 23:56:37 -0300

    Node.js

---
+++ Nível 2/HelloWorld.js
@@ -0,0 +1,2 @@
+console.log("Hello World")
+console.log("Me livrei da maldição")
---
+++ Nível 2/Variáveis.js
@@ -0,0 +1,38 @@
+
+
+// Declararação de Variável
+
+const Num = 1+1
+const Frase = "O Reino em Branco não faz promessas para o amanhã"
+const Float = (2/3)
+
+
+// Teste de Variável
+console.log(Num)
+console.log(Frase)
+console.log(Float)
+
+//Booleano
+const ver = 3+3
+const fa = 3*3
+console.log(ver >  fa)
+
+
+//Verificar tipo de variável
+console.log(typeof Num)
+console.log(typeof Frase)
+console.log(typeof Float)
+console.log(typeof ver > fa)
+
+
+// Array e Object
+
+const Lista = [1, 2, 3]
+const pessoa = {nome: "Isabela", idade: 16}
+
+console.log(Lista[0])
+console.log(typeof Lista)
+console.log(pessoa)
+console.log(pessoa.nome)
+console.log(typeof pessoa)
+
