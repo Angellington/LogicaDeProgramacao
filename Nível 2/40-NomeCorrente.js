8e40f196c75841066ea8ab6bf84900e60dffb5a9
Yukenshi7230 <61129075+Yukenshi7230@users.noreply.github.com>
2024-04-24 23:44:25 -0300

    Dia5

---
+++ Nível 2/40-NomeCorrente.js
@@ -0,0 +1,17 @@
+/*  Faça um programa que crie um vetor para abrigar 5 nomes de pessoas. 
+Também crie uma variável que guardará a posição de um vetor. 
+Depois, indique que a posição é 3 e que nesta posição deve ser armazenado o nome Amanda. 
+Exiba na tela o conteúdo da posição corrente. */
+
+console.clear()
+
+/*  Faça um programa que crie um vetor para abrigar 5 nomes de pessoas. */
+let nomes = ['Isabela', 'Camila', 'Matheus', "Batata", 'Angel']
+
+/*Também crie uma variável que guardará a posição de um vetor. */
+let posNomes = nomes[3]
+console.log(posNomes)
+/*Depois, indique que a posição é 3 e que nesta posição deve ser armazenado o nome Amanda. */
+
+
+
