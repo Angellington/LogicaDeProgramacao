93d287fec9261580abb00e52d3a37f7a74c28cf5
Yukenshi7230 <61129075+Angellington7230@users.noreply.github.com>
2024-05-02 23:05:26 -0300

    mit

---
+++ JS-Test/modal/script.js
@@ -0,0 +1,10 @@
+const texto = document.querySelector('h1')
+const button = document.querySelector('button')
+
+texto.onclick = function () {
+    alert('Oi')
+}
+
+document.addEventListener(button.onclick = function () {
+    alert('oi')
+}) 
---
+++ Nível 3/JS-DOM/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+   const document = 'document'
+
+const button = document.querySelector("button")
+
+button.onclick = function () {
+    console.log("Oi")
+}
+});
