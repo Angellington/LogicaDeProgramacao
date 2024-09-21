75298da0525d3279c29d70d00225bcfec845a117
Yukenshi7230 <61129075+Angellington7230@users.noreply.github.com>
2024-09-21 16:20:24 -0300

    Start with 50 challenger

---
+++ JS-Test/Nivel 4 - Basics/1-Odd-Numbers/index.js
@@ -0,0 +1,13 @@
+// Contagem de Números Pares: Conte quantos números pares existem entre 1 e 50.
+// Number Odds Counts: Count how many odd number is in 1 and 50
+
+// Make a for with 50 numbers
+const numbers = []
+
+for(let i = 1; i < 52; i++){
+    if (i % 2 === 0)
+    console.log(i)
+}
+
+
+
