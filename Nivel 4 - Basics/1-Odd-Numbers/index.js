020685f76403339f002d40208c561afc8b7ee6ae
Yukenshi7230 <61129075+Angellington7230@users.noreply.github.com>
2024-09-22 19:33:43 -0300

    Basics Challenger #1

--- Nivel 4 - Basics/1-Odd-Numbers/index.js
+++ Nivel 4 - Basics/1-Odd-Numbers/index.js
@@ -2,12 +2,26 @@
 // Number Odds Counts: Count how many odd number is in 1 and 50
 
 // Make a for with 50 numbers
-const numbers = []
+console.clear()
 
-for(let i = 1; i < 52; i++){
-    if (i % 2 === 0)
-    console.log(i)
+const numbers = [];
+
+// for(let i = 0; i < 51; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+
+let n =  0;
+
+while (n < 50){
+    n++
+    if(n  % 2 ===0){
+        numbers.push(n)
+    } else {
+        continue
+    }
 }
 
-
-
+console.log(numbers)
